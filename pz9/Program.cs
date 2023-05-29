59e1c578cac63ee8337443ea253a1b8b2a7edec0
Tatarinov_Artem_3PK1 <103313928+flagik228@users.noreply.github.com>
2023-05-29 08:32:41 +0300

    Add files via upload

---
+++ pz9/Program.cs
@@ -0,0 +1,64 @@
+﻿{
+    Adapter adapter = new Adapter();
+    Client client = new Client(adapter);
+    client.Show();
+
+
+}
+class Original
+{
+    public void OriginalDouble(double a)
+    {
+        Console.WriteLine($"Вывод числа double: {a}");
+    }
+    public void OriginalInt(int b)
+    {
+        Console.WriteLine($"Вывод числа int: {b}");
+    }
+    public void OriginalChar(char c)
+    {
+        Console.WriteLine($"Вывод символа 5 раз: {c}");
+    }
+}
+interface ITarget
+{
+    void ClientDouble(double a);
+    void ClientInt(int b);
+    void ClientChar(char c);
+}
+class Adapter : Original, ITarget
+{
+    public void ClientDouble(double a)
+    {
+        OriginalDouble(a);
+    }
+
+    public void ClientInt(int b)
+    {
+        OriginalInt(b * 2);
+    }
+
+    public void ClientChar(char c)
+    {
+        for (int i = 0; i <= 4; i++)
+        {
+            OriginalChar(c);
+        }
+
+    }
+}
+class Client
+{
+    private ITarget client;
+
+    public Client(ITarget _client)
+    {
+        client = _client;
+    }
+    public void Show()
+    {
+        client.ClientDouble(12.32);
+        client.ClientInt(123);
+        client.ClientChar('A');
+    }
+}
