99f8a6d5be32b964dfebae98cf6ac0f17f765966
Tatarinov_Artem_3PK1 <103313928+flagik228@users.noreply.github.com>
2023-05-29 08:30:48 +0300

    Add files via upload

---
+++ pz3/Program.cs
@@ -0,0 +1,30 @@
+﻿{
+    Employee e1 = new Employee("Полкина Анна Сергеевна", 19);
+    Employee e2 = (Employee)e1.Clone();
+    Console.WriteLine(e1.CompareTo(e2));
+    Console.WriteLine(e2.ToString());
+    Console.ReadKey();
+}
+        internal class Employee : ICloneable, IComparable<Employee>
+{
+    private string FIO { get; set; }
+    private int Age { get; set; }
+    public Employee(string fio, int age)
+    {
+        FIO = fio;
+        Age = age;
+    }
+    public object Clone()
+    {
+        return new Employee(this.FIO, this.Age);
+    }
+    public int CompareTo(Employee? o)
+    {
+        if (o is Employee employee) return FIO.CompareTo(employee.FIO);
+        else throw new ArgumentException("Некорректное значение параметра");
+    }
+    public override string ToString()
+    {
+        return new string($"ФИО: {FIO}\n" + $"Возраст: {Age}\n");
+    }
+}
