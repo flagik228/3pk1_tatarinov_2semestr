fd1b7b0123acb0e59f94f9d651b27370191859b2
Tatarinov_Artem_3PK1 <103313928+flagik228@users.noreply.github.com>
2023-05-29 08:29:40 +0300

    Add files via upload

---
+++ pz2/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int n = 5;
+int[,] a = new int[,]
+{
+               {0, 1, 1, 0, 0},
+               {0, 0, 0, 1, 0},
+               {0, 1, 0, 0, 1},
+               {0, 0, 1, 0, 0},
+               {0, 0, 0, 1, 0}
+};
+
+for (int k = 0; k < n; k++)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i, k] == a[k, j])
+            {
+                a[i, j] = 1;
+            }
+        }
+    }
+}
+Console.Write("Матрица достижимости:\n");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(a[i, j]);
+    }
+    Console.WriteLine();
+}
